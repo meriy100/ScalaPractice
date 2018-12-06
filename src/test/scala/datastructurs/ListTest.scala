bbf8aa77a5bf03ff96823e6400233ebee9d86043
meriy100 <ttattataa@gmail.com>
2018-12-06 20:32:47 +0900

    [Add] Exercise3.1

--- src/test/scala/datastructurs/ListTest.scala
+++ src/test/scala/datastructurs/ListTest.scala
@@ -6,4 +6,9 @@ class ListTest extends FunSuite with Matchers {
   test("testList") {
     List.sum(List(1, 2)) should equal (List.sum(Cons(1, Cons(2, Nil))))
   }
+
+  test("testSum") {
+    List.sum(Nil) should equal(0)
+    List.sum(Cons(1, List(1, 2))) should equal(1 + List.sum(List(1, 2)))
+  }
 }
