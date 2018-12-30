7a5f7659bcb820d8c2556de3c01d5166418a599b
meriy100 <ttattataa@gmail.com>
2018-12-30 20:42:38 +0900

    [Add] exercise 4.6

--- src/main/scala/errorhandling/package.scala
+++ src/main/scala/errorhandling/package.scala
@@ -1,10 +1,32 @@
 package errorhandling
 
-sealed trait Either[+E, +A]
+sealed trait Either[+E, +A] {
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Left(e) => Left(e)
+    case Right(a) => Right(f(a))
+  }
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Left(e) => Left(e)
+    case Right(a) => f(a)
+  }
+
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Left(_) => b
+    case Right(a) => Right(a)
+  }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    for {
+      aa <- this
+      bb <- b
+    } yield f(aa, bb)
+}
 case class Left[+E](value: E) extends Either[E, Nothing]
 case class Right[+A](value: A) extends Either[Nothing, A]
 
 object Either {
+
   def Try[A](a: => A): Either[Exception, A] =
     try Right(a)
     catch { case e: Exception => Left(e) }
--- src/test/scala/errorhandling/EitherTest.scala
+++ src/test/scala/errorhandling/EitherTest.scala
@@ -3,6 +3,29 @@ package errorhandling
 import org.scalatest._
 
 class EitherTest extends FunSuite with Matchers {
+  test("testMap2") {
+    val mean1 = Either.mean(Vector(1,2,3,4,5))
+    val mean2 = Either.mean(Vector(1,2,3,4))
+    mean1.map2(mean2)(_ + _) should equal(Right(5.5))
+  }
+
+  test("testOrElse") {
+    Either.mean(Vector(1,2,3,4,5)) orElse (Left("This is left!")) should equal(Right(3.0))
+    Either.mean(Vector(1,2,3,4)) orElse (Left("This is left!")) should equal(Right(2.5))
+    Either.mean(Vector()) orElse (Left("This is left!")) should equal(Left("This is left!"))
+  }
+
+  test("testFlatMap") {
+    Either.mean(Vector(1,2,3,4,5)) flatMap (x => Right(x.toString())) should equal(Right("3.0"))
+    Either.mean(Vector(1,2,3,4,5)) flatMap (_ => Left("This is left!")) should equal(Left("This is left!"))
+    Either.mean(Vector()) flatMap (x => Right(x.toString())) should equal(Left("mean of empty list!"))
+  }
+
+  test("testMap") {
+    Either.mean(Vector(1,2,3,4,5)) map (_.toString()) should equal(Right("3.0"))
+    Either.mean(Vector()) map (_.toString()) should equal(Left("mean of empty list!"))
+  }
+
   test("testTry") {
     Either.Try(4/2) should equal(Right(2))
     Either.Try(4/0) match {
