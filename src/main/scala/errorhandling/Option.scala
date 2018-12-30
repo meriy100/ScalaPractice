1e868f7d3e07c60bc7656678e6a8b2c543972708
meriy100 <ttattataa@gmail.com>
2018-12-30 16:29:29 +0900

    [{Add] exercise 4.1

--- src/main/scala/errorhandling/Option.scala
+++ src/main/scala/errorhandling/Option.scala
@@ -1,5 +1,30 @@
 package errorhandling
 
+sealed trait Option[+A] {
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(v) => Some(f(v))
+  }
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case None => default
+    case Some(v) => v
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] =
+    map(f) getOrElse None
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] =
+    map(Some(_)) getOrElse(ob)
+
+  def filter(f: A => Boolean): Option[A] =
+    if (map(f) getOrElse(false)) this
+    else None
+
+}
+case class Some[+A](get: A) extends Option[A]
+case object None extends Option[Nothing]
+
 object Option {
   def failingFn(i: Int):Int = {
     val y: Int = throw new Exception("fail!")
@@ -22,4 +47,12 @@ object Option {
     if (xs.isEmpty)
       throw new ArithmeticException("mean of empty list!")
     else xs.sum / xs.length
+
+  def mean2(xs: Seq[Double], onEmpty: Double): Double =
+    if (xs.isEmpty) onEmpty
+    else xs.sum / xs.length
+
+  def mean3(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
 }
--- src/test/scala/errorhandling/OptionTest.scala
+++ src/test/scala/errorhandling/OptionTest.scala
@@ -3,6 +3,40 @@ package errorhandling
 import org.scalatest._
 
 class OptionTest extends FunSuite with Matchers {
+  test("testFilter") {
+    Some(12) filter (i => i > 10) should equal(Some(12))
+    Some(10).filter((i:Int) => i > 10) should equal(None)
+    None.filter((i:Int) => i > 10) should equal(None)
+  }
+
+  test("testOrElse") {
+    Some(12) orElse Some(1211) should equal(Some(12))
+    None orElse Some(1211) should equal(Some(1211))
+  }
+
+  test("testFlatMap") {
+    Some(12) flatMap (v => Some(v.toString())) should equal(Some("12"))
+    None flatMap (v => Some(v.toString())) should equal(None)
+  }
+
+  test("testGetOrElse") {
+    Some(12) getOrElse 0 should equal(12)
+    None getOrElse (0) should equal(0)
+  }
+
+  test("testMap") {
+    Some(12) map (v => v.toString()) should equal(Some("12"))
+    None map (v => v.toString()) should equal(None)
+  }
+
+  test("testMean3") {
+    Option.mean3(List(1.0,2.0,3.0,4.0)) should equal(Some(2.5))
+    Option.mean3(List()) should equal(None)
+  }
+  test("testMean2") {
+    Option.mean2(List(1.0,2.0,3.0,4.0), 0.0/0.0) should equal(2.5)
+    Option.mean2(List(), 0.0) should equal(0.0)
+  }
   test("testMean") {
     Option.mean(List(1.0,2.0,3.0,4.0)) should equal(2.5)
     the [ArithmeticException] thrownBy {
